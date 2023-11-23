584cff919674932ad8144f01fb866cffcb4f04ae
Дима Чуйко <dimachy26@gmail.com>
2023-11-23 18:43:46 +0300

    Registration and Authorization

---
+++ app/src/main/java/com/example/myapplication/dto/UsersData.java
@@ -0,0 +1,101 @@
+package com.example.myapplication.dto;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class UsersData implements Serializable {
+
+
+    private int userId;
+    private String firstName;
+    private String secondName;
+    private String lastName;
+    private String birthdate;
+    private int age;
+    private String number;
+    private String login;
+    private String password;
+    private String email;
+
+    public int getIdFamily() {
+        return userId;
+    }
+
+    public void setIdFamily(int idFamily) {
+        this.userId = idFamily;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
